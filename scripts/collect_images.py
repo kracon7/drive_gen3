a9f3643d4ca61e567a2c8135e36fbe1ab31698aa
Jiacheng <ethancooper007@gmail.com>
2021-03-21 16:10:16 -0500

    tested fixed camera image align

--- scripts/collect_images.py
+++ scripts/collect_images.py
@@ -10,6 +10,11 @@ import numpy as np
 #     sys.path.remove('/opt/ros/kinetic/lib/python2.7/dist-packages')
 # import cv2
 from sensor_msgs.msg import Image
+import pyrealsense2 as rs
+import matplotlib.pyplot as plt
+
+plt.ion()
+fig, ax = plt.subplots(2,1)
 
 
 name_to_dtypes = {
@@ -51,9 +56,79 @@ def image_to_numpy(msg):
 rospy.init_node('camera_test')
 rospy.loginfo("testing virtual env")
 
-print('here')
 data = rospy.wait_for_message('/camera/color/image_raw', Image)
 
 np_image = image_to_numpy(data)
 
 print(np_image.shape)
+
+# Create a pipeline
+pipeline = rs.pipeline()
+
+# Create a config and configure the pipeline to stream
+#  different resolutions of color and depth streams
+config = rs.config()
+
+# Get device product line for setting a supporting resolution
+pipeline_wrapper = rs.pipeline_wrapper(pipeline)
+pipeline_profile = config.resolve(pipeline_wrapper)
+device = pipeline_profile.get_device()
+device_product_line = str(device.get_info(rs.camera_info.product_line))
+
+config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+
+if device_product_line == 'L500':
+    config.enable_stream(rs.stream.color, 960, 540, rs.format.bgr8, 30)
+else:
+    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+
+# Start streaming
+profile = pipeline.start(config)
+
+# Getting the depth sensor's depth scale (see rs-align example for explanation)
+depth_sensor = profile.get_device().first_depth_sensor()
+depth_scale = depth_sensor.get_depth_scale()
+print("Depth Scale is: " , depth_scale)
+
+# We will be removing the background of objects more than
+#  clipping_distance_in_meters meters away
+clipping_distance_in_meters = 1 #1 meter
+clipping_distance = clipping_distance_in_meters / depth_scale
+
+# Create an align object
+# rs.align allows us to perform alignment of depth frames to others frames
+# The "align_to" is the stream type to which we plan to align depth frames.
+align_to = rs.stream.color
+align = rs.align(align_to)
+
+
+while True:
+    # Get frameset of color and depth
+    frames = pipeline.wait_for_frames()
+    # frames.get_depth_frame() is a 640x360 depth image
+
+    # Align the depth frame to color frame
+    aligned_frames = align.process(frames)
+
+    # Get aligned frames
+    aligned_depth_frame = aligned_frames.get_depth_frame() # aligned_depth_frame is a 640x480 depth image
+    color_frame = aligned_frames.get_color_frame()
+
+    # Validate that both frames are valid
+    if not aligned_depth_frame or not color_frame:
+        continue
+
+    depth_image = np.asanyarray(aligned_depth_frame.get_data())
+    color_image = np.asanyarray(color_frame.get_data())
+
+    # Remove background - Set pixels further than clipping_distance to grey
+    grey_color = 153
+    depth_image_3d = np.dstack((depth_image,depth_image,depth_image)) #depth image is 1 channel, color is 3 channels
+    bg_removed = np.where((depth_image_3d > clipping_distance) | (depth_image_3d <= 0), grey_color, color_image)
+
+    ax[0].imshow(color_image)
+    ax[1].imshow(depth_image)
+    plt.pause(0.01)
+
+
+pipeline.stop()
